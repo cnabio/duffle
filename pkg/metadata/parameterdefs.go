d82f22ccc1dbd2d234a7fcfdf8197a2dff5d11bf
itowlson <github@hestia.cc>
2018-08-21 10:58:36 +1200

    Parameter definitions deserialiser (#48)

---
+++ pkg/metadata/parameterdefs.go
@@ -0,0 +1,103 @@
+package metadata
+
+import (
+	"fmt"
+)
+
+// ParameterDefinition defines a single parameter for a CNAB bundle
+type ParameterDefinition struct {
+	DataType      string            `json:"type"`
+	DefaultValue  interface{}       `json:"defaultValue"`
+	AllowedValues []interface{}     `json:"allowedValues"`
+	MinValue      *int              `json:"minValue"`
+	MaxValue      *int              `json:"maxValue"`
+	MinLength     *int              `json:"minLength"`
+	MaxLength     *int              `json:"maxLength"`
+	Metadata      ParameterMetadata `json:"metadata"`
+}
+
+// ParameterMetadata contains metadata for a parameter definition.
+type ParameterMetadata struct {
+	Description string `json:"description"`
+}
+
+// ValidateParameterValue checks whether a value is valid as the value of
+// the specified parameter.
+func (pd ParameterDefinition) ValidateParameterValue(value interface{}) error {
+	if len(pd.AllowedValues) > 0 {
+		if !isInCollection(value, pd.AllowedValues) {
+			return fmt.Errorf("Value is not in the set of allowed values for this parameter")
+		}
+	}
+
+	switch pd.DataType {
+	case "string":
+		return pd.validateStringParameterValue(value)
+	case "int":
+		return pd.validateIntParameterValue(value)
+	case "bool":
+		return pd.validateBoolParameterValue(value)
+	default:
+		return fmt.Errorf("Invalid parameter definition")
+	}
+}
+
+func (pd ParameterDefinition) validateStringParameterValue(value interface{}) error {
+	s, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("Value is not a string")
+	}
+	if pd.MinLength != nil && len(s) < *pd.MinLength {
+		return fmt.Errorf("Value is too short: minimum length is %d", pd.MinLength)
+	}
+	if pd.MaxLength != nil && len(s) > *pd.MaxLength {
+		return fmt.Errorf("Value is too long: maximum length is %d", pd.MaxLength)
+	}
+	return nil
+}
+
+func (pd ParameterDefinition) validateIntParameterValue(value interface{}) error {
+	i, ok := value.(int)
+	if !ok {
+		f, ok := value.(float64)
+		if !ok {
+			return fmt.Errorf("Value is not a number")
+		}
+		i, ok = asInt(f)
+		if !ok {
+			return fmt.Errorf("Value is not an integer")
+		}
+	}
+	if pd.MinValue != nil && i < *pd.MinValue {
+		return fmt.Errorf("Value is too low: minimum value is %d", pd.MinValue)
+	}
+	if pd.MaxValue != nil && i > *pd.MaxValue {
+		return fmt.Errorf("Value is too long: maximum length is %d", pd.MaxValue)
+	}
+	return nil
+}
+
+func (pd ParameterDefinition) validateBoolParameterValue(value interface{}) error {
+	_, ok := value.(bool)
+	if !ok {
+		return fmt.Errorf("Value is not a string")
+	}
+	return nil
+}
+
+func isInCollection(value interface{}, values []interface{}) bool {
+	for _, v := range values {
+		if value == v {
+			return true
+		}
+	}
+	return false
+}
+
+func asInt(f float64) (int, bool) {
+	i := int(f)
+	if float64(i) != f {
+		return 0, false
+	}
+	return i, true
+}
---
+++ pkg/metadata/parameterdefs_test.go
@@ -0,0 +1,332 @@
+package metadata
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCanReadParameterNames(t *testing.T) {
+	json := `{
+		"parameters": {
+			"foo": { },
+			"bar": { }
+		}
+	}`
+	definitions, err := Parse(json)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(definitions.Parameters) != 2 {
+		t.Fatalf("Expected 2 parameter definitons, got %d", len(definitions.Parameters))
+	}
+	if _, ok := definitions.Parameters["foo"]; !ok {
+		t.Errorf("Expected an entry with name 'foo' but didn't get one")
+	}
+	if _, ok := definitions.Parameters["bar"]; !ok {
+		t.Errorf("Expected an entry with name 'bar' but didn't get one")
+	}
+}
+
+func TestCanReadParameterDefinition(t *testing.T) {
+	dataType := "int"
+	defaultValue := "some default"
+	allowedValues0 := "foo"
+	allowedValues1 := "bar"
+	minValue := 100
+	maxValue := 200
+	minLength := 300
+	maxLength := 400
+	description := "some description"
+	json := fmt.Sprintf(`{
+		"parameters": {
+			"test": {
+				"type": "%s",
+				"defaultValue": "%s",
+				"allowedValues": [ "%s", "%s" ],
+				"minValue": %d,
+				"maxValue": %d,
+				"minLength": %d,
+				"maxLength": %d,
+				"metadata": {
+					"description": "%s"
+				}
+			}
+		}
+	}`,
+		dataType, defaultValue, allowedValues0, allowedValues1,
+		minValue, maxValue, minLength, maxLength, description)
+
+	definitions, err := Parse(json)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	p := definitions.Parameters["test"]
+	if p.DataType != dataType {
+		t.Errorf("Expected data type '%s' but got '%s'", dataType, p.DataType)
+	}
+	if p.DefaultValue != defaultValue {
+		t.Errorf("Expected default value '%s' but got '%s'", defaultValue, p.DefaultValue)
+	}
+	if len(p.AllowedValues) != 2 {
+		t.Errorf("Expected 2 allowed values but got %d", len(p.AllowedValues))
+	}
+	if p.AllowedValues[0] != allowedValues0 {
+		t.Errorf("Expected allowed value '%s' but got '%s'", allowedValues0, p.AllowedValues[0])
+	}
+	if p.AllowedValues[1] != allowedValues1 {
+		t.Errorf("Expected allowed value '%s' but got '%s'", allowedValues1, p.AllowedValues[1])
+	}
+	if *p.MinValue != minValue {
+		t.Errorf("Expected min value '%d' but got '%d'", minValue, p.MinValue)
+	}
+	if *p.MinLength != minLength {
+		t.Errorf("Expected min length '%d' but got '%d'", minLength, p.MinLength)
+	}
+	if *p.MaxValue != maxValue {
+		t.Errorf("Expected max value '%d' but got '%d'", maxValue, p.MaxValue)
+	}
+	if *p.MaxLength != maxLength {
+		t.Errorf("Expected max length '%d' but got '%d'", maxLength, p.MaxLength)
+	}
+	if p.Metadata.Description != description {
+		t.Errorf("Expected description '%s' but got '%s'", description, p.Metadata.Description)
+	}
+}
+
+func valueTestJSON(jsonRepresentation string) string {
+	return fmt.Sprintf(`{
+		"parameters": {
+			"test": {
+				"defaultValue": %s,
+				"allowedValues": [ %s ]
+			}
+		}
+	}`, jsonRepresentation, jsonRepresentation)
+}
+
+func expectString(desc string, expected string, o interface{}, t *testing.T) {
+	s, ok := o.(string)
+	if !ok {
+		t.Errorf("Expected string %s but didn't get one", desc)
+	}
+	if s != expected {
+		t.Errorf("Expected %s '%s' but got '%s'", desc, expected, s)
+	}
+}
+
+func expectInt(desc string, expected int, o interface{}, t *testing.T) {
+	i, ok := o.(float64)
+	if !ok {
+		t.Errorf("Expected int %s but didn't get one", desc)
+	}
+	if int(i) != expected {
+		t.Errorf("Expected %s '%d' but got '%d'", desc, expected, int(i))
+	}
+}
+
+func expectBool(desc string, expected bool, o interface{}, t *testing.T) {
+	b, ok := o.(bool)
+	if !ok {
+		t.Errorf("Expected bool %s but didn't get one", desc)
+	}
+	if b != expected {
+		t.Errorf("Expected %s '%t' but got '%t'", desc, expected, b)
+	}
+}
+
+func TestCanReadValues(t *testing.T) {
+	strValue := "\"some string\""
+	intValue := "123"
+	boolValue := "true"
+
+	strDef, err := Parse(valueTestJSON(strValue))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectString("default value", "some string", strDef.Parameters["test"].DefaultValue, t)
+	expectString("allowed value", "some string", strDef.Parameters["test"].AllowedValues[0], t)
+
+	intDef, err := Parse(valueTestJSON(intValue))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectInt("default value", 123, intDef.Parameters["test"].DefaultValue, t)
+	expectInt("allowed value", 123, intDef.Parameters["test"].AllowedValues[0], t)
+
+	boolDef, err := Parse(valueTestJSON(boolValue))
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectBool("default value", true, boolDef.Parameters["test"].DefaultValue, t)
+	expectBool("allowed value", true, boolDef.Parameters["test"].AllowedValues[0], t)
+}
+
+func TestValidateStringParameterValue_AnyAllowed(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType: "string",
+	}
+
+	err := pd.ValidateParameterValue("foo")
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue(17)
+	if err == nil {
+		t.Errorf("Expected invalid type but got no error")
+	}
+}
+
+func TestValidateStringParameterValue_AllowedOnly(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType:      "string",
+		AllowedValues: []interface{}{"foo", "bar"},
+	}
+
+	err := pd.ValidateParameterValue("foo")
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue("quux")
+	if err == nil {
+		t.Errorf("Expected disallowed value but got no error")
+	}
+}
+
+func TestValidateStringParameterValue_MinLength(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType:  "string",
+		MinLength: intPtr(5),
+	}
+
+	err := pd.ValidateParameterValue("foobar")
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue("foo")
+	if err == nil {
+		t.Errorf("Expected too-short value but got no error")
+	}
+}
+
+func TestValidateStringParameterValue_MaxLength(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType:  "string",
+		MaxLength: intPtr(5),
+	}
+
+	err := pd.ValidateParameterValue("foo")
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue("foobar")
+	if err == nil {
+		t.Errorf("Expected too-long value but got no error")
+	}
+}
+
+func TestValidateIntParameterValue_AnyAllowed(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType: "int",
+	}
+
+	err := pd.ValidateParameterValue(17)
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue(float64(17))
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue(17.5)
+	if err == nil {
+		t.Errorf("Expected not an integer but got no error")
+	}
+
+	err = pd.ValidateParameterValue("17")
+	if err == nil {
+		t.Errorf("Expected invalid type but got no error")
+	}
+}
+
+func TestValidateIntParameterValue_AllowedOnly(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType:      "int",
+		AllowedValues: []interface{}{17, 23},
+	}
+
+	err := pd.ValidateParameterValue(17)
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue(58)
+	if err == nil {
+		t.Errorf("Expected disallowed value but got no error")
+	}
+}
+
+func TestValidateIntParameterValue_MinValue(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType: "int",
+		MinValue: intPtr(5),
+	}
+
+	err := pd.ValidateParameterValue(17)
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue(3)
+	if err == nil {
+		t.Errorf("Expected too-small value but got no error")
+	}
+}
+
+func TestValidateIntParameterValue_MaxValue(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType: "int",
+		MaxValue: intPtr(5),
+	}
+
+	err := pd.ValidateParameterValue(3)
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue(17)
+	if err == nil {
+		t.Errorf("Expected too-large value but got no error")
+	}
+}
+
+func TestValidateBoolParameterValue(t *testing.T) {
+	pd := ParameterDefinition{
+		DataType: "bool",
+	}
+
+	err := pd.ValidateParameterValue(true)
+	if err != nil {
+		t.Errorf("Expected valid but got error %s", err)
+	}
+
+	err = pd.ValidateParameterValue(17)
+	if err == nil {
+		t.Errorf("Expected invalid type but got no error")
+	}
+
+	err = pd.ValidateParameterValue("17")
+	if err == nil {
+		t.Errorf("Expected invalid type but got no error")
+	}
+}
+
+func intPtr(i int) *int {
+	return &i
+}
