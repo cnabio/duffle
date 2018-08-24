395ab272904c1f796394d25b95695cfffb101f1e
Matt Butcher <matt.butcher@microsoft.com>
2018-08-23 18:00:13 -0600

    feat: allow Docker to mount local tmpdirs for credentials.

--- pkg/driver/docker_driver.go
+++ pkg/driver/docker_driver.go
@@ -2,8 +2,10 @@ package driver
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 // DockerDriver is capable of running Docker invocation images using Docker itself.
@@ -15,8 +17,7 @@ type DockerDriver struct {
 
 // Run executes the Docker driver
 func (d *DockerDriver) Run(op *Operation) error {
-	// TODO: add op.files
-	return d.exec(op.Image, op.Environment)
+	return d.exec(op)
 }
 
 // Handles indicates that the Docker driver supports "docker" and "oci"
@@ -24,13 +25,53 @@ func (d *DockerDriver) Handles(dt string) bool {
 	return dt == ImageTypeDocker || dt == ImageTypeOCI
 }
 
-func (d *DockerDriver) exec(img string, env map[string]string) error {
+func (d *DockerDriver) exec(op *Operation) error {
+	img := op.Image
+	env := op.Environment
 	// FIXME: This is all temporary code. We should really just link the Docker library and
 	// directly send this.
 	args := []string{"run"}
 	for k, v := range env {
 		args = append(args, "-e", fmt.Sprintf("%s=%v", k, v))
 	}
+
+	// To pass secrets into the running container, we loop through all of the files
+	// and store them in a local temp directory (one file per directory). Then we
+	// mount all of the directories to Docker.
+	tmpdirs := map[string]string{}
+	defer func() {
+		for _, tmp := range tmpdirs {
+			os.RemoveAll(tmp)
+		}
+	}()
+	for path, content := range op.Files {
+		path, err := filepath.Abs(path)
+		if err != nil {
+			return err
+		}
+		base := filepath.Base(path)
+		dir := filepath.Dir(path)
+
+		// If it's another file in the same folder, add it to the existing tmp location
+		if existingTmp, ok := tmpdirs[base]; ok {
+			ioutil.WriteFile(filepath.Join(existingTmp, base), []byte(content), 0755)
+			continue
+		}
+
+		// FIXME: On Docker-for-Mac, you must hard code the temp dir location. But
+		// does this work on Windows?
+		tmp, err := ioutil.TempDir("/tmp", "duffle-volume-")
+		if err != nil {
+			return err
+		}
+		tmpdirs[dir] = tmp
+		localFile := filepath.Join(tmp, base)
+		if err := ioutil.WriteFile(localFile, []byte(content), 0755); err != nil {
+			fmt.Println(err)
+		}
+		args = append(args, "--volume", fmt.Sprintf("%s:%s", tmp, dir))
+	}
+
 	// TODO: Should we hard code in the call to run? This might actually make it possible
 	// for CNAB devs to create a default command that is perhaps user-oriented (like setting
 	// the default command to help text).
