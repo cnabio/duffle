2e7d3498585313ea78280b68c5e85e644f1fbc4c
Michelle Noorali <michellemolu@gmail.com>
2018-09-04 13:57:42 -0400

    feelin a little volunerable over here

--- brigade.js
+++ brigade.js
@@ -15,7 +15,7 @@ function checkRequested(e, p) {
   }
 
   // This will represent our build job. For us, it's just an empty thinger.
-  const build = new Job("build", "alpine:3.7", ["sleep 60", "echo pls work bro"])
+  const build = new Job("build", "alpine:3.7", ["sleep 60", "echo hey"])
 
   // For convenience, we'll create three jobs: one for each GitHub Check
   // stage.
