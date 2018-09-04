6bc0b1596cb2c1f9c7b8199f519a6b7ae56a7e34
Michelle Noorali <michellemolu@gmail.com>
2018-09-04 10:49:07 -0400

    add a test brigade.js file

---
+++ brigade.js
@@ -0,0 +1,51 @@
+const {events, Job, Group} = require("brigadier");
+const checkRunImage = "technosophos/brigade-github-check-run:latest"
+
+events.on("check_suite:requested", checkRequested)
+events.on("check_suite:rerequested", checkRequested)
+events.on("check_run:rerequested", checkRequested)
+
+function checkRequested(e, p) {
+  console.log("check requested")
+  // Common configuration
+  const env = {
+    CHECK_PAYLOAD: e.payload,
+    CHECK_NAME: "MyService",
+    CHECK_TITLE: "Echo Testing",
+  }
+
+  // This will represent our build job. For us, it's just an empty thinger.
+  const build = new Job("build", "alpine:3.7", ["sleep 60", "echo hey"])
+
+  // For convenience, we'll create three jobs: one for each GitHub Check
+  // stage.
+  const start = new Job("start-run", checkRunImage)
+  start.imageForcePull = true
+  start.env = env
+  start.env.CHECK_SUMMARY = "Beginning test run"
+
+  const end = new Job("end-run", checkRunImage)
+  end.imageForcePull = true
+  end.env = env
+
+  // Now we run the jobs in order:
+  // - Notify GitHub of start
+  // - Run the test
+  // - Notify GitHub of completion
+  //
+  // On error, we catch the error and notify GitHub of a failure.
+  start.run().then(() => {
+    return build.run()
+  }).then( (result) => {
+    end.env.CHECK_CONCLUSION = "success"
+    end.env.CHECK_SUMMARY = "Build completed"
+    end.env.CHECK_TEXT = result.toString()
+    return end.run()
+  }).catch( (err) => {
+    // In this case, we mark the ending failed.
+    end.env.CHECK_CONCLUSION = "failed"
+    end.env.CHECK_SUMMARY = "Build failed"
+    end.env.CHECK_TEXT = `Error: ${ err }`
+    return end.run()
+  })
+}
