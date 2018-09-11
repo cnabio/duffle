d233cbd0b285e3d1bf5280a59b07876e062f83f0
Radu Matei <matei.radu94@gmail.com>
2018-09-11 18:02:28 +0300

    Initial Docker client for driver

--- pkg/driver/docker_driver.go
+++ pkg/driver/docker_driver.go
@@ -1,11 +1,19 @@
 package driver
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"path/filepath"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/strslice"
+	"github.com/docker/docker/client"
 )
 
 // DockerDriver is capable of running Docker invocation images using Docker itself.
@@ -25,80 +33,6 @@ func (d *DockerDriver) Handles(dt string) bool {
 	return dt == ImageTypeDocker || dt == ImageTypeOCI
 }
 
-func (d *DockerDriver) exec(op *Operation) error {
-	img := op.Image
-	env := op.Environment
-	// FIXME: This is all temporary code. We should really just link the Docker library and
-	// directly send this.
-	args := []string{"run"}
-	for k, v := range env {
-		args = append(args, "-e", fmt.Sprintf("%s=%v", k, v))
-	}
-
-	// To pass secrets into the running container, we loop through all of the files
-	// and store them in a local temp directory (one file per directory). Then we
-	// mount all of the directories to Docker.
-	tmpdirs := map[string]string{}
-	defer func() {
-		for _, tmp := range tmpdirs {
-			os.RemoveAll(tmp)
-		}
-	}()
-	for path, content := range op.Files {
-		path, err := filepath.Abs(path)
-		if err != nil {
-			return err
-		}
-		base := filepath.Base(path)
-		dir := filepath.Dir(path)
-
-		// If it's another file in the same folder, add it to the existing tmp location
-		if existingTmp, ok := tmpdirs[base]; ok {
-			ioutil.WriteFile(filepath.Join(existingTmp, base), []byte(content), 0755)
-			continue
-		}
-
-		// FIXME: On Docker-for-Mac, you must hard code the temp dir location. But
-		// does this work on Windows?
-		tmp, err := ioutil.TempDir("/tmp", "duffle-volume-")
-		if err != nil {
-			return err
-		}
-		tmpdirs[dir] = tmp
-		localFile := filepath.Join(tmp, base)
-		if err := ioutil.WriteFile(localFile, []byte(content), 0755); err != nil {
-			fmt.Println(err)
-		}
-		args = append(args, "--volume", fmt.Sprintf("%s:%s", tmp, dir))
-	}
-
-	// TODO: Should we hard code in the call to run? This might actually make it possible
-	// for CNAB devs to create a default command that is perhaps user-oriented (like setting
-	// the default command to help text).
-	args = append(args, img, "/cnab/app/run")
-
-	if isTrue(d.config["VERBOSE"]) {
-		fmt.Println("--------> args")
-		for _, arg := range args {
-			fmt.Println(arg)
-		}
-		fmt.Println("<-------- args")
-	}
-
-	if d.Simulate {
-		return nil
-	}
-	var err error
-	cmd := exec.Command("docker", args...)
-	cmd.Dir, err = os.Getwd()
-	if err != nil {
-		return err
-	}
-	out, err := cmd.CombinedOutput()
-	fmt.Println("\n" + string(out) + "\n")
-	return err
-}
-
 // Config returns the Docker driver configuration options
 func (d *DockerDriver) Config() map[string]string {
 	return map[string]string{
@@ -109,4 +43,114 @@ func (d *DockerDriver) Config() map[string]string {
 // SetConfig sets Docker driver configuration
 func (d *DockerDriver) SetConfig(settings map[string]string) {
 	d.config = settings
+}
+
+func (d *DockerDriver) exec(op *Operation) error {
+
+	// TODO - should ctx be passed here?
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.38"))
+	if err != nil {
+		return fmt.Errorf("cannot create Docker client: %v", err)
+	}
+
+	if d.Simulate {
+		return nil
+	}
+
+	// TODO - decide how to handle logs from Docker
+	_, err = cli.ImagePull(ctx, op.Image, types.ImagePullOptions{})
+	if err != nil {
+		return fmt.Errorf("cannot pull image %v: %v", op.Image, err)
+	}
+
+	var env []string
+	for k, v := range op.Environment {
+		env = append(env, fmt.Sprintf("%s=%v", k, v))
+	}
+
+	var mounts []mount.Mount
+
+	// To pass secrets into the running container, we loop through all of the files
+	// and store them in a local temp directory (one file per directory). Then we
+	// mount all of the directories to Docker.
+	tmpdirs := map[string]string{}
+	defer func() {
+		for _, tmp := range tmpdirs {
+			os.RemoveAll(tmp)
+		}
+	}()
+
+	for path, content := range op.Files {
+		// osPath is used to compute files to create
+		// based on the operating system running duffle install
+		osPath, err := filepath.Abs(path)
+		if err != nil {
+			return err
+		}
+		base := filepath.Base(osPath)
+		dir := filepath.Dir(osPath)
+
+		// mount is the target mount path in the container
+		//
+		// TODO - make sure this is actually computed correctly
+		// as the filepath.Dir(osPath) for any Unix path
+		var m string
+		x := strings.Split(path, `/`)
+		if len(x) > 0 {
+			m = strings.Join(x[:len(x)-1], "/")
+		} else {
+			m = strings.Join(x, "/")
+		}
+
+		// If it's another file in the same folder, add it to the existing tmp location
+		if existingTmp, ok := tmpdirs[base]; ok {
+			ioutil.WriteFile(filepath.Join(existingTmp, base), []byte(content), 0755)
+			continue
+		}
+
+		tmp, err := ioutil.TempDir(os.TempDir(), "duffle-volume-")
+		if err != nil {
+			return err
+		}
+		tmpdirs[dir] = tmp
+		localFile := filepath.Join(tmp, base)
+		if err := ioutil.WriteFile(localFile, []byte(content), 0755); err != nil {
+			fmt.Println(err)
+		}
+
+		mounts = append(mounts, mount.Mount{Type: mount.TypeBind, Source: tmp, Target: m})
+	}
+
+	cfg := &container.Config{
+		Image:      op.Image,
+		Entrypoint: strslice.StrSlice{"/cnab/app/run"},
+	}
+
+	hostCfg := &container.HostConfig{Mounts: mounts}
+
+	resp, err := cli.ContainerCreate(ctx, cfg, hostCfg, nil, "")
+	if err != nil {
+		return fmt.Errorf("cannot create container: %v", err)
+	}
+
+	if err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return fmt.Errorf("cannot start container: %v", err)
+	}
+
+	statusc, errc := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errc:
+		if err != nil {
+			return fmt.Errorf("error in container: %v", err)
+		}
+	case <-statusc:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true, ShowStderr: true})
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(out)
+	fmt.Println(buf.String())
+
+	return nil
 }
