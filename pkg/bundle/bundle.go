00d050b323040227126239a20b16b9fa7c61aae0
Matthew Fisher <matt.fisher@microsoft.com>
2018-09-18 15:54:48 -0700

    feat(build): inject parameters and credentials
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- pkg/bundle/bundle.go
+++ pkg/bundle/bundle.go
@@ -18,36 +18,36 @@ func Parse(text string) (Bundle, error) {
 
 // LocationRef specifies a location within the invocation package
 type LocationRef struct {
-	Path  string `json:"path"`
-	Field string `json:"field"`
+	Path  string `json:"path" toml:"path"`
+	Field string `json:"field" toml:"field"`
 }
 
 // Image describes a container image in the bundle
 type Image struct {
-	Name string        `json:"name"`
-	URI  string        `json:"uri"`
-	Refs []LocationRef `json:"refs"`
+	Name string        `json:"name" toml:"name"`
+	URI  string        `json:"uri" toml:"uri"`
+	Refs []LocationRef `json:"refs" toml:"refs"`
 }
 
 // InvocationImage contains the image type and location for the installation of a bundle
 type InvocationImage struct {
-	ImageType string `json:"imageType"`
-	Image     string `json:"image"`
+	ImageType string `json:"imageType" toml:"imageType"`
+	Image     string `json:"image" toml:"image"`
 }
 
 // CredentialLocation provides the location of a credential that the invocation
 // image needs to use.
 type CredentialLocation struct {
-	Path                string `json:"path"`
-	EnvironmentVariable string `json:"env"`
+	Path                string `json:"path" toml:"path"`
+	EnvironmentVariable string `json:"env" toml:"env"`
 }
 
 // Bundle is a CNAB metadata document
 type Bundle struct {
-	Name            string                         `json:"name"`
-	Version         string                         `json:"version"`
-	InvocationImage InvocationImage                `json:"invocationImage"`
-	Images          []Image                        `json:"images"`
-	Parameters      map[string]ParameterDefinition `json:"parameters"`
-	Credentials     map[string]CredentialLocation  `json:"credentials"`
+	Name            string                         `json:"name" toml:"name"`
+	Version         string                         `json:"version" toml:"version"`
+	InvocationImage InvocationImage                `json:"invocationImage" toml:"invocationImage"`
+	Images          []Image                        `json:"images" toml:"images"`
+	Parameters      map[string]ParameterDefinition `json:"parameters" toml:"parameters"`
+	Credentials     map[string]CredentialLocation  `json:"credentials" toml:"credentials"`
 }
