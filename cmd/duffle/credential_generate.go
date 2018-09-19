5e13ab8f5258f814931911e230c2f916384af0ca
Matt Butcher <matt.butcher@microsoft.com>
2018-09-19 09:14:13 -0600

    fix: add long doc to duffle creds gen

--- cmd/duffle/credential_generate.go
+++ cmd/duffle/credential_generate.go
@@ -14,12 +14,29 @@ import (
 	"github.com/deis/duffle/pkg/duffle/home"
 )
 
+const credentialGenerateHelp = `Generate credentials from a CNAB bundle
+
+This reads a bundle.json file's credential requirements and generates a stub credentialset.
+The given name becomes the name of the credentialset.
+
+If a bundle is given, the bundle may be fetched (unless there is a cached copy),
+and will then be examined. If the '-f' flag is specified, though, it will read the
+bundle.json supplied.
+
+The generated credentials will all be initialed to stub values, and should be edited
+to reflect the true values.
+
+The newly created credential set will be added to the credentialsets, though users
+will still need to edit that file to set the appropriate values.
+`
+
 func newCredentialGenerateCmd(out io.Writer) *cobra.Command {
 	bundleFile := ""
 	cmd := &cobra.Command{
 		Use:     "generate NAME [BUNDLE]",
 		Aliases: []string{"gen"},
 		Short:   "generate a credentialset from a bundle",
+		Long:    credentialGenerateHelp,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			bf, err := bundleFileOrArg2(args, bundleFile, out)
 			if err != nil {
