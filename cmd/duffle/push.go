794697951aed3a9f391a77a06d11e7e8b80f9b35
Radu Matei <matei.radu94@gmail.com>
2018-09-22 01:06:54 +0300

    Add naive implementation to push a thin bundle

--- cmd/duffle/push.go
+++ cmd/duffle/push.go
@@ -1,23 +1,72 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
+	"github.com/deis/duffle/pkg/duffle/home"
 	"github.com/spf13/cobra"
 )
 
-// TODO
-func newPushCmd(w io.Writer) *cobra.Command {
-	const usage = `TODO`
+type pushCmd struct {
+	out  io.Writer
+	src  string
+	home home.Home
+}
+
+func newPushCmd(out io.Writer) *cobra.Command {
+	const usage = `pushes a CNAB bundle to a repository`
+
+	var push = &pushCmd{out: out}
 
 	cmd := &cobra.Command{
 		Use:   "push",
 		Short: usage,
 		Long:  usage,
-		Run: func(cmd *cobra.Command, args []string) {
-			unimplemented("duffle push")
+		RunE: func(_ *cobra.Command, args []string) error {
+			if len(args) > 0 {
+				push.src = args[0]
+			}
+			push.home = home.Home(homePath())
+			return push.run()
 		},
 	}
 
 	return cmd
+}
+
+func (p *pushCmd) run() error {
+	bundlePath := filepath.Join(p.src, "cnab", "bundle.json")
+	b, err := loadBundle(bundlePath)
+	if err != nil {
+		return err
+	}
+
+	bundleParts := strings.Split(b.Name, "/")
+	repo := bundleParts[0]
+	name := bundleParts[1]
+
+	// TODO - decide on the api here - bundles vs. repositories
+	//
+	// this is the case of a thin bundle, where only the bundle file is pushed
+	url := fmt.Sprintf("https://%s/bundles/%s.json", repo, name)
+
+	body, err := os.Open(bundlePath)
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
 }
