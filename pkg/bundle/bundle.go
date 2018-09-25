42bae0604f45ad3ee45ade7a0acc08662ccbb54a
Ivan Towlson <itowlson@microsoft.com>
2018-09-25 14:39:22 +1200

    Fixed some issues with numeric parameters and improved errors

--- pkg/bundle/bundle.go
+++ pkg/bundle/bundle.go
@@ -2,6 +2,7 @@ package bundle
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 // ParseBuffer reads CNAB metadata out of a JSON byte stream
@@ -57,10 +58,12 @@ func ValuesOrDefaults(vals map[string]interface{}, b *Bundle) (map[string]interf
 	res := map[string]interface{}{}
 	for name, def := range b.Parameters {
 		if val, ok := vals[name]; ok {
-			if err := def.ValidateParameterValue(val); err != nil {
-				return res, err
+			err := def.ValidateParameterValue(val)
+			if err != nil {
+				return res, fmt.Errorf("can't use %v as value of %s: %s", val, name, err)
 			}
-			res[name] = val
+			typedVal := def.CoerceValue(val)
+			res[name] = typedVal
 			continue
 		}
 		res[name] = def.DefaultValue
