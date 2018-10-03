9c206bed0c9d6db5ef24ca7a2e850f52e6e09c00
Matt Butcher <matt.butcher@microsoft.com>
2018-10-03 11:38:17 -0600

    feat: add signature library (#255)
    * feat: signatures for bundles
    
    * add tests and testdata
    
    * add Add and Save methods on keyring
    
    Also, add a passphrase fetcher to the keyring, and have that fetcher passed to every key.
    
    * Spelling and unit test fixes

---
+++ pkg/signature/signature_test.go
@@ -0,0 +1,69 @@
+package signature
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/deis/duffle/pkg/bundle"
+)
+
+func TestSigner_Sign(t *testing.T) {
+	is := assert.New(t)
+	k := getKey(keyEmail, t)
+	is.NotNil(k)
+
+	b := &bundle.Bundle{
+		Name:    "mybundle",
+		Version: "1.2.3",
+	}
+
+	// Sign the bundle twice and make sure the sigs are the same.
+	s := NewSigner(k)
+	sig1, err := s.Clearsign(b)
+	is.NoError(err)
+	sig2, err := s.Clearsign(b)
+	is.NoError(err)
+	is.Equal(sig1, sig2)
+	is.NotEmpty(sig1)
+
+	// Verify that the returned identities are the same
+	ring, err := LoadKeyRing(keyringFile)
+	is.NoError(err)
+	v := NewVerifier(ring)
+	signedBy, err := v.Verify(sig1)
+	is.NoError(err)
+	id1 := k.entity.Identities[fullKeyID]
+	id2 := signedBy.entity.Identities[fullKeyID]
+	is.NotNil(id1)
+	is.NotNil(id2)
+	is.Equal(id1.Name, id2.Name)
+}
+
+func TestSigner_Attest(t *testing.T) {
+	is := assert.New(t)
+	k := getKey(keyEmail, t)
+	is.NotNil(k)
+
+	b := &bundle.Bundle{
+		Name:    "mybundle",
+		Version: "1.2.3",
+	}
+
+	// Sign the bundle twice and make sure the sigs are the same.
+	s := NewSigner(k)
+	sig, err := s.Clearsign(b)
+	attestation, err := s.Attest(sig)
+	is.NoError(err)
+	is.Contains(string(sig), string(attestation))
+}
+
+func getKey(keyname string, t *testing.T) *Key {
+	k, err := LoadKeyRing(keyringFile)
+	assert.NoError(t, err)
+
+	key, err := k.Key(keyname)
+	key.PassphraseFetcher = testPassphraseFetch
+	assert.NoError(t, err)
+	return key
+}
---
+++ pkg/signature/verifier_test.go
@@ -0,0 +1,74 @@
+package signature
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	signedFile       = "testdata/signed.json.asc"
+	failedSignedFile = "testdata/fail-signed.json.asc"
+)
+
+func TestVerifier_Verify(t *testing.T) {
+	is := assert.New(t)
+
+	data, err := ioutil.ReadFile(signedFile)
+	is.NoError(err)
+
+	keyring, err := LoadKeyRing(keyringFile)
+	is.NoError(err)
+
+	v := NewVerifier(keyring)
+	k, err := v.Verify(data)
+	is.NoError(err)
+	is.NotNil(k.entity.Identities[key2Email])
+}
+func TestVerifier_VerifyFail(t *testing.T) {
+	is := assert.New(t)
+
+	data, err := ioutil.ReadFile(failedSignedFile)
+	is.NoError(err)
+
+	keyring, err := LoadKeyRing(keyringFile)
+	is.NoError(err)
+
+	// The file was signed by a user not in the keyring, so should fail.
+	v := NewVerifier(keyring)
+	_, err = v.Verify(data)
+	is.Error(err)
+	is.Contains(err.Error(), "signature made by unknown entity")
+}
+
+func TestVerifier_Extract(t *testing.T) {
+	is := assert.New(t)
+
+	data, err := ioutil.ReadFile(signedFile)
+	is.NoError(err)
+
+	keyring, err := LoadKeyRing(keyringFile)
+	is.NoError(err)
+	v := NewVerifier(keyring)
+
+	b, k, err := v.Extract(data)
+	is.NoError(err)
+	is.NotNil(k.entity.Identities[key2Email])
+	is.Equal("example_bundle", b.Name)
+}
+
+func TestVerifier_ExtractFail(t *testing.T) {
+	is := assert.New(t)
+
+	data, err := ioutil.ReadFile(failedSignedFile)
+	is.NoError(err)
+
+	keyring, err := LoadKeyRing(keyringFile)
+	is.NoError(err)
+	v := NewVerifier(keyring)
+
+	_, _, err = v.Extract(data)
+	is.Error(err)
+	is.Contains(err.Error(), "signature made by unknown entity")
+}
