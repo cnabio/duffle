67241a209c677f61d1354846e64651e58f4f4051
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-10-15 14:43:50 -0600

    Updated credential_generate.go to replace error message
    The credential_generate command reuses the bundleFileOrArg2 function. That function, in install.go, has a couple of install references embedded within it. This PR introduces a new function for the `credentials generate` command.
    
    Fixes #282

--- cmd/duffle/main_test.go
+++ cmd/duffle/main_test.go
@@ -10,10 +10,10 @@ import (
 func TestIsPathy(t *testing.T) {
 	is := assert.New(t)
 	for path, expect := range map[string]bool{
-		"foo":                                    false,
+		"foo": false,
 		filepath.Join("this", "is", "a", "path"): true,
-		"foo.yaml":                               false,
-		filepath.Join("..", "foo.yaml"):          true,
+		"foo.yaml":                      false,
+		filepath.Join("..", "foo.yaml"): true,
 	} {
 		is.Equal(expect, isPathy(path), "Expected %t, for %s", expect, path)
 	}
