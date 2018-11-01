99846f8d41e53d7fae0ec71c9ebf87c31c1cf16f
Radu Matei <matei.radu94@gmail.com>
2018-11-01 22:33:58 +0200

    Use Docker client for driver instead of exec (#185)
    * Initial Docker client for driver
    
    * Update to use HOME dir, docker attach
    
    * Lint fix
    
    * Update contaienr attach handling
    
    * Added docker socket
    
    * Updated to use Docker's CopyToContainer
    
    * Lint fixes
    
    * Hardcoded isTerm to false, refactored tar build
    
    * Updated docker client creation
    
    * Removed commented import
    
    * Formatting fix
    
    Co-authored-by: Jeremy Rickard <jeremy.rickard@microsoft.com>

--- pkg/driver/docker_driver.go
+++ pkg/driver/docker_driver.go
@@ -1,14 +1,21 @@
 package driver
 
 import (
-	"errors"
+	"archive/tar"
+	"context"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
-	"os/exec"
 	unix_path "path"
-	"path/filepath"
-	"runtime"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/api/types/strslice"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/docker/pkg/term"
 )
 
 // DockerDriver is capable of running Docker invocation images using Docker itself.
@@ -28,87 +35,6 @@ func (d *DockerDriver) Handles(dt string) bool {
 	return dt == ImageTypeDocker || dt == ImageTypeOCI
 }
 
-func (d *DockerDriver) exec(op *Operation) error {
-	img := op.Image
-	env := op.Environment
-	// FIXME: This is all temporary code. We should really just link the Docker library and
-	// directly send this.
-	args := []string{"run"}
-	for k, v := range env {
-		args = append(args, "-e", fmt.Sprintf("%s=%v", k, v))
-	}
-
-	// To pass secrets into the running container, we loop through all of the files
-	// and store them in a local temp directory (one file per directory). Then we
-	// mount all of the directories to Docker.
-	tmpdirs := map[string]string{}
-	defer func() {
-		for _, tmp := range tmpdirs {
-			os.RemoveAll(tmp)
-		}
-	}()
-	for path, content := range op.Files {
-		if !unix_path.IsAbs(path) {
-			return errors.New("destination path should be an absolute unix path")
-		}
-		base := unix_path.Base(path)
-		dir := unix_path.Dir(path)
-
-		// If it's another file in the same folder, add it to the existing tmp location
-		if existingTmp, ok := tmpdirs[base]; ok {
-			ioutil.WriteFile(filepath.Join(existingTmp, base), []byte(content), 0755)
-			continue
-		}
-
-		tmpDirRoot := "/tmp"
-		if runtime.GOOS == "windows" {
-			tmpDirRoot = ""
-		}
-		tmp, err := ioutil.TempDir(tmpDirRoot, "duffle-volume-")
-		if err != nil {
-			return err
-		}
-		tmpdirs[dir] = tmp
-		localFile := filepath.Join(tmp, base)
-		if err := ioutil.WriteFile(localFile, []byte(content), 0755); err != nil {
-			fmt.Fprintln(op.Out, err)
-		}
-		args = append(args, "--volume", fmt.Sprintf("%s:%s", localFile, fmt.Sprintf("%s/%s", dir, base)))
-	}
-
-	// TODO: For now, we mount the docker socket to alllow things like Compose
-	// to run inside of a CNAB bundle. This should be configurable.
-	// See https://github.com/docker/compose/blob/master/script/run/run.sh
-	// Also https://media.giphy.com/media/RIECDaCdxqKha/giphy.gif
-	args = append(args, "--volume", "/var/run/docker.sock:/var/run/docker.sock")
-
-	// TODO: Should we hard code in the call to run? This might actually make it possible
-	// for CNAB devs to create a default command that is perhaps user-oriented (like setting
-	// the default command to help text).
-	args = append(args, img, "/cnab/app/run")
-
-	if isTrue(d.config["VERBOSE"]) {
-		fmt.Fprintln(op.Out, "--------> args")
-		for _, arg := range args {
-			fmt.Fprintln(op.Out, arg)
-		}
-		fmt.Fprintln(op.Out, "<-------- args")
-	}
-
-	if d.Simulate {
-		return nil
-	}
-	var err error
-	cmd := exec.Command("docker", args...)
-	cmd.Dir, err = os.Getwd()
-	if err != nil {
-		return err
-	}
-	out, err := cmd.CombinedOutput()
-	fmt.Fprintln(op.Out, "\n"+string(out)+"\n")
-	return err
-}
-
 // Config returns the Docker driver configuration options
 func (d *DockerDriver) Config() map[string]string {
 	return map[string]string{
@@ -119,4 +45,126 @@ func (d *DockerDriver) Config() map[string]string {
 // SetConfig sets Docker driver configuration
 func (d *DockerDriver) SetConfig(settings map[string]string) {
 	d.config = settings
+}
+
+func (d *DockerDriver) exec(op *Operation) error {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("cannot create Docker client: %v", err)
+	}
+	cli.NegotiateAPIVersion(ctx)
+	if err != nil {
+		return fmt.Errorf("cannot update Docker client: %v", err)
+	}
+	if d.Simulate {
+		return nil
+	}
+	fmt.Println("Pulling Invocation Image...")
+	pullReader, err := cli.ImagePull(ctx, op.Image, types.ImagePullOptions{})
+	if err != nil {
+		return fmt.Errorf("cannot pull image %v: %v", op.Image, err)
+	}
+	termFd, _ := term.GetFdInfo(os.Stdout)
+	// Setting this to false here because Moby os.Exit(1) all over the place and this fails on WSL (only)
+	// when Term is true.
+	isTerm := false
+	err = jsonmessage.DisplayJSONMessagesStream(pullReader, os.Stdout, termFd, isTerm, nil)
+	if err != nil {
+		return err
+	}
+	var env []string
+	for k, v := range op.Environment {
+		env = append(env, fmt.Sprintf("%s=%v", k, v))
+	}
+
+	mounts := []mount.Mount{
+		{
+			Type:   mount.TypeBind,
+			Source: "/var/run/docker.sock",
+			Target: "/var/run/docker.sock",
+		},
+	}
+	cfg := &container.Config{
+		Image:      op.Image,
+		Env:        env,
+		Entrypoint: strslice.StrSlice{"/cnab/app/run"},
+	}
+
+	hostCfg := &container.HostConfig{Mounts: mounts, AutoRemove: true}
+
+	resp, err := cli.ContainerCreate(ctx, cfg, hostCfg, nil, "")
+	if err != nil {
+		return fmt.Errorf("cannot create container: %v", err)
+	}
+
+	tarContent, err := generateTar(op.Files)
+	if err != nil {
+		return fmt.Errorf("error staging files: %s", err)
+	}
+	options := types.CopyToContainerOptions{
+		AllowOverwriteDirWithFile: false,
+	}
+	// This copies the tar to the root of the container. The tar has been assembled using the
+	// path from the given file, starting at the /.
+	err = cli.CopyToContainer(ctx, resp.ID, "/", tarContent, options)
+	if err != nil {
+		return fmt.Errorf("error copying to / in container: %s", err)
+	}
+
+	if err = cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return fmt.Errorf("cannot start container: %v", err)
+	}
+
+	attach, err := cli.ContainerAttach(ctx, resp.ID, types.ContainerAttachOptions{
+		Stream: true,
+		Stdout: true,
+		Stderr: true,
+		Logs:   true,
+	})
+	if err != nil {
+		return fmt.Errorf("unable to retrieve logs: %v", err)
+	}
+	go func() {
+		defer attach.Close()
+		for {
+			_, err := stdcopy.StdCopy(os.Stdout, os.Stderr, attach.Reader)
+			if err != nil {
+				break
+			}
+		}
+	}()
+
+	statusc, errc := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errc:
+		if err != nil {
+			return fmt.Errorf("error in container: %v", err)
+		}
+	case <-statusc:
+	}
+	return err
+}
+
+func generateTar(files map[string]string) (io.Reader, error) {
+	r, w := io.Pipe()
+	tw := tar.NewWriter(w)
+	for path := range files {
+		if !unix_path.IsAbs(path) {
+			return nil, fmt.Errorf("destination path %s should be an absolute unix path", path)
+		}
+	}
+	go func() {
+		for path, content := range files {
+			hdr := &tar.Header{
+				Name: path,
+				Mode: 0644,
+				Size: int64(len(content)),
+			}
+			tw.WriteHeader(hdr)
+			tw.Write([]byte(content))
+		}
+		w.Close()
+	}()
+	return r, nil
 }
