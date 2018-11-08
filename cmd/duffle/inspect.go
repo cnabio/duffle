606526b3d0dad0406c05a3f8786c35e04641b8bd
Radu Matei <radu@radu-matei.com>
2018-11-08 15:47:09 +0200

    Enable duffle inspect for remote bundles
    Add argument for inspect message
    
    Add usage and arg count check for inspect
    
    Cleanup

--- cmd/duffle/inspect.go
+++ cmd/duffle/inspect.go
@@ -1,42 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/spf13/cobra"
-
-	"github.com/deis/duffle/pkg/duffle/home"
-	"github.com/deis/duffle/pkg/repo"
 )
 
 func newInspectCmd(w io.Writer) *cobra.Command {
+	var (
+		insecure bool
+	)
+
+	const usage = ` Returns information about an application bundle.
+
+	Example:
+		$ duffle inspect duffle/example:0.1.0
+
+	To inspect unsigned bundles, pass the --insecure flag:
+		$ duffle inspect duffle/unsinged-example:0.1.0 --insecure
+`
+
 	cmd := &cobra.Command{
-		Use:   "inspect",
+		Use:   "inspect NAME",
 		Short: "return low-level information on application bundles",
+		Long:  usage,
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			home := home.Home(homePath())
 			bundleName := args[0]
 
-			ref, err := getReference(bundleName)
-			if err != nil {
-				return fmt.Errorf("could not parse reference for %s: %v", bundleName, err)
-			}
-
-			// read the bundle reference from repositories.json
-			index, err := repo.LoadIndex(home.Repositories())
-			if err != nil {
-				return fmt.Errorf("cannot open %s: %v", home.Repositories(), err)
-			}
-
-			digest, err := index.Get(ref.Name(), ref.Tag())
+			bundle, err := loadOrPullBundle(bundleName, insecure)
 			if err != nil {
 				return err
 			}
 
-			f, err := os.Open(filepath.Join(home.Bundles(), digest))
+			f, err := os.Open(bundle)
 			if err != nil {
 				return err
 			}
@@ -47,6 +45,9 @@ func newInspectCmd(w io.Writer) *cobra.Command {
 			return nil
 		},
 	}
+
+	flags := cmd.Flags()
+	flags.BoolVarP(&insecure, "insecure", "k", false, "Do not verify the bundle (INSECURE)")
 
 	return cmd
 }
