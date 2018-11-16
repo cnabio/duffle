df4a8120ea01c0ead05679570334177482029845
Vaughn Dice <vadice@microsoft.com>
2018-11-16 14:41:47 -0700

    ref(brigade.js): consistent indentation

--- brigade.js
+++ brigade.js
@@ -15,8 +15,8 @@ function build(e, project) {
 
   // Set a few environment variables.
   build.env = {
-      "DEST_PATH": localPath,
-      "GOPATH": gopath
+    "DEST_PATH": localPath,
+    "GOPATH": gopath
   };
 
   // Run Go unit tests
@@ -43,86 +43,86 @@ function runSuite(e, p) {
 
 // runTests is a Check Run that is ran as part of a Checks Suite
 function runTests(e, p) {
-    console.log("Check requested")
+  console.log("Check requested")
 
-    // Create Notification object (which is just a Job to update GH using the Checks API)
-    var note = new Notification(`tests`, e, p);
-    note.conclusion = "";
-    note.title = "Run Tests";
-    note.summary = "Running the test targets for " + e.revision.commit;
-    note.text = "This test will ensure build, linting and tests all pass."
+  // Create Notification object (which is just a Job to update GH using the Checks API)
+  var note = new Notification(`tests`, e, p);
+  note.conclusion = "";
+  note.title = "Run Tests";
+  note.summary = "Running the test targets for " + e.revision.commit;
+  note.text = "This test will ensure build, linting and tests all pass."
 
-    // Send notification, then run, then send pass/fail notification
-    return notificationWrap(build(e, p), note)
+  // Send notification, then run, then send pass/fail notification
+  return notificationWrap(build(e, p), note)
 }
 
 // A GitHub Check Suite notification
 class Notification {
   constructor(name, e, p) {
-      this.proj = p;
-      this.payload = e.payload;
-      this.name = name;
-      this.externalID = e.buildID;
-      // TODO: add Kashti link when available
-      // this.detailsURL = `https://<kashti domain>/kashti/builds/${ e.buildID }`;
-      this.title = "running check";
-      this.text = "";
-      this.summary = "";
+    this.proj = p;
+    this.payload = e.payload;
+    this.name = name;
+    this.externalID = e.buildID;
+    // TODO: add Kashti link when available
+    // this.detailsURL = `https://<kashti domain>/kashti/builds/${ e.buildID }`;
+    this.title = "running check";
+    this.text = "";
+    this.summary = "";
 
-      // count allows us to send the notification multiple times, with a distinct pod name
-      // each time.
-      this.count = 0;
+    // count allows us to send the notification multiple times, with a distinct pod name
+    // each time.
+    this.count = 0;
 
-      // One of: "success", "failure", "neutral", "cancelled", or "timed_out".
-      this.conclusion = "neutral";
+    // One of: "success", "failure", "neutral", "cancelled", or "timed_out".
+    this.conclusion = "neutral";
   }
 
   // Send a new notification, and return a Promise<result>.
   run() {
-      this.count++
-      var j = new Job(`${ this.name }-${ this.count }`, "deis/brigade-github-check-run:latest");
-      j.imageForcePull = true;
-      j.env = {
-          CHECK_CONCLUSION: this.conclusion,
-          CHECK_NAME: this.name,
-          CHECK_TITLE: this.title,
-          CHECK_PAYLOAD: this.payload,
-          CHECK_SUMMARY: this.summary,
-          CHECK_TEXT: this.text,
-          // TODO: add when applicable
-          // CHECK_DETAILS_URL: this.detailsURL,
-          CHECK_EXTERNAL_ID: this.externalID
-      }
-      return j.run();
+    this.count++
+    var j = new Job(`${ this.name }-${ this.count }`, "deis/brigade-github-check-run:latest");
+    j.imageForcePull = true;
+    j.env = {
+      CHECK_CONCLUSION: this.conclusion,
+      CHECK_NAME: this.name,
+      CHECK_TITLE: this.title,
+      CHECK_PAYLOAD: this.payload,
+      CHECK_SUMMARY: this.summary,
+      CHECK_TEXT: this.text,
+      // TODO: add when applicable
+      // CHECK_DETAILS_URL: this.detailsURL,
+      CHECK_EXTERNAL_ID: this.externalID
+    }
+    return j.run();
   }
 }
 
 // Helper to wrap a job execution between two notifications.
 async function notificationWrap(job, note, conclusion) {
   if (conclusion == null) {
-      conclusion = "success"
+    conclusion = "success"
   }
   await note.run();
   try {
-      let res = await job.run()
-      const logs = await job.logs();
+    let res = await job.run()
+    const logs = await job.logs();
 
-      note.conclusion = conclusion;
-      note.summary = `Task "${ job.name }" passed`;
-      note.text = note.text = "```" + res.toString() + "```\nTest Complete";
-      return await note.run();
+    note.conclusion = conclusion;
+    note.summary = `Task "${ job.name }" passed`;
+    note.text = note.text = "```" + res.toString() + "```\nTest Complete";
+    return await note.run();
   } catch (e) {
-      const logs = await job.logs();
-      note.conclusion = "failure";
-      note.summary = `Task "${ job.name }" failed for ${ e.buildID }`;
-      note.text = "```" + logs + "```\nFailed with error: " + e.toString();
-      try {
-          return await note.run();
-      } catch (e2) {
-          console.error("failed to send notification: " + e2.toString());
-          console.error("original error: " + e.toString());
-          return e2;
-      }
+    const logs = await job.logs();
+    note.conclusion = "failure";
+    note.summary = `Task "${ job.name }" failed for ${ e.buildID }`;
+    note.text = "```" + logs + "```\nFailed with error: " + e.toString();
+    try {
+      return await note.run();
+    } catch (e2) {
+      console.error("failed to send notification: " + e2.toString());
+      console.error("original error: " + e.toString());
+      return e2;
+    }
   }
 }
 
@@ -235,7 +235,7 @@ function acrBuild(project, tag) {
 function slackNotify(title, msg, project) {
   if (project.secrets.SLACK_WEBHOOK) {
     var slack = new Job(`${projectName}-slack-notify`, "technosophos/slack-notify:latest")
-    slack.imageForcePull = true;
+
     slack.env = {
       SLACK_WEBHOOK: project.secrets.SLACK_WEBHOOK,
       SLACK_USERNAME: "duffle-ci",
