c71a6278b13ef250bcc039168f079aefcfaf0053
Simon Ferquel <simon.ferquel@docker.com>
2018-11-29 00:25:18 +0100

    Docker driver fixes (#533)
    * Introduce quiet mode for Docker Driver
    
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>
    
    * Ensure Stderr is streamed alongside stdout
    
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>
    
    * Avoid making the driver always quiet
    
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- pkg/driver/docker_driver.go
+++ pkg/driver/docker_driver.go
@@ -41,8 +41,9 @@ func (d *DockerDriver) Handles(dt string) bool {
 // Config returns the Docker driver configuration options
 func (d *DockerDriver) Config() map[string]string {
 	return map[string]string{
-		"VERBOSE":     "Increase verbosity. true, false are supported values",
-		"PULL_ALWAYS": "Always pull image, even if locally available (0|1)",
+		"VERBOSE":             "Increase verbosity. true, false are supported values",
+		"PULL_ALWAYS":         "Always pull image, even if locally available (0|1)",
+		"DOCKER_DRIVER_QUIET": "Make the Docker driver quiet (only print container stdout/stderr)",
 	}
 }
 
@@ -83,9 +84,20 @@ func pullImage(ctx context.Context, cli command.Cli, image string) error {
 		nil)
 }
 
+type nullWriter struct{}
+
+func (nullWriter) Write(b []byte) (int, error) {
+	return len(b), nil
+}
+
 func (d *DockerDriver) exec(op *Operation) error {
 	ctx := context.Background()
-	cli := command.NewDockerCli(os.Stdin, os.Stdout, os.Stderr, false)
+	var cliout, clierr io.Writer = os.Stdout, os.Stderr
+	if d.config["DOCKER_DRIVER_QUIET"] == "1" {
+		cliout = nullWriter{}
+		clierr = nullWriter{}
+	}
+	cli := command.NewDockerCli(os.Stdin, cliout, clierr, false)
 	if err := cli.Initialize(cliflags.NewClientOptions()); err != nil {
 		return err
 	}
@@ -110,9 +122,11 @@ func (d *DockerDriver) exec(op *Operation) error {
 		},
 	}
 	cfg := &container.Config{
-		Image:      op.Image,
-		Env:        env,
-		Entrypoint: strslice.StrSlice{"/cnab/app/run"},
+		Image:        op.Image,
+		Env:          env,
+		Entrypoint:   strslice.StrSlice{"/cnab/app/run"},
+		AttachStderr: true,
+		AttachStdout: true,
 	}
 
 	hostCfg := &container.HostConfig{Mounts: mounts, AutoRemove: true}
