afd25f66d9bf30523595ff79f0f2ffa872eb46ce
Ryan Moran <rmoran@pivotal.io>
2019-02-12 11:32:14 -0500

    Refactors show command into standalone struct
    - this common struct can then be shared between the `duffle show` and
    `duffle bundle show` commands

--- cmd/duffle/show.go
+++ cmd/duffle/show.go
@@ -2,63 +2,29 @@ package main
 
 import (
 	"io"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
 func newShowCmd(w io.Writer) *cobra.Command {
-	var (
-		insecure bool
-		raw      bool
-	)
-
-	const usage = ` Returns information about an application bundle.
-
-	Example:
-		$ duffle show duffle/example:0.1.0
-
-	To display unsigned bundles, pass the --insecure flag:
-		$ duffle show duffle/unsinged-example:0.1.0 --insecure
-`
+	bsc := &bundleShowCmd{}
+	bsc.w = w
 
 	cmd := &cobra.Command{
 		Use:   "show NAME",
 		Short: "return low-level information on application bundles",
-		Long:  usage,
+		Long:  bsc.usage(false),
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			bundleName := args[0]
+			bsc.name = args[0]
 
-			bundleFile, err := getBundleFilepath(bundleName, homePath(), insecure)
-			if err != nil {
-				return err
-			}
-
-			if raw {
-				f, err := os.Open(bundleFile)
-				if err != nil {
-					return err
-				}
-				defer f.Close()
-				_, err = io.Copy(w, f)
-				return err
-			}
-
-			bun, err := loadBundle(bundleFile, insecure)
-			if err != nil {
-				return err
-			}
-
-			_, err = bun.WriteTo(w)
-
-			return err
+			return bsc.run()
 		},
 	}
 
 	flags := cmd.Flags()
-	flags.BoolVarP(&insecure, "insecure", "k", false, "Do not verify the bundle (INSECURE)")
-	flags.BoolVarP(&raw, "raw", "r", false, "Display the raw bundle manifest")
+	flags.BoolVarP(&bsc.insecure, "insecure", "k", false, "Do not verify the bundle (INSECURE)")
+	flags.BoolVarP(&bsc.raw, "raw", "r", false, "Display the raw bundle manifest")
 
 	return cmd
 }
