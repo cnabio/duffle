d696a296f8f64d4cfb1b13394621e35ef26928c5
Glyn Normington <gnormington@pivotal.io>
2019-06-13 10:47:01 +0100

    duffle export should no longer store images in tar files
    Thus the --oci-layout flag is no longer needed.
    
    Ref https://github.com/deislabs/duffle/issues/767

--- cmd/duffle/export.go
+++ cmd/duffle/export.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/pkg/errors"
-
 	"github.com/deislabs/duffle/pkg/duffle/home"
 	"github.com/deislabs/duffle/pkg/loader"
 	"github.com/deislabs/duffle/pkg/packager"
@@ -17,12 +15,7 @@ const exportDesc = `
 Packages a bundle, and by default any images referenced by the bundle, within
 a single gzipped tarfile.
 
-If neither --oci-layout nor --thin is specified, all images (incuding invocation
-images) referenced by the bundle metadata are saved as tar files in the
-artifacts/ directory along with an artifacts.json file which describes the
-contents of the artifacts/ directory.
-
-If --oci-layout is specified, all images (incuding invocation images) referenced
+If --thin is not specified, all images (incuding invocation images) referenced
 by the bundle metadata are saved as an OCI image layout in the artifacts/layout/
 directory.
 
@@ -47,7 +40,6 @@ type exportCmd struct {
 	thin         bool
 	verbose      bool
 	bundleIsFile bool
-	ociLayout    bool
 }
 
 func newExportCmd(w io.Writer) *cobra.Command {
@@ -61,9 +53,6 @@ func newExportCmd(w io.Writer) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			export.home = home.Home(homePath())
 			export.bundle = args[0]
-			if export.thin && export.ociLayout {
-				return errors.New("--thin and --oci-layout must not both be specified")
-			}
 
 			return export.run()
 		},
@@ -72,7 +61,6 @@ func newExportCmd(w io.Writer) *cobra.Command {
 	f := cmd.Flags()
 	f.StringVarP(&export.dest, "output-file", "o", "", "Save exported bundle to file path")
 	f.BoolVarP(&export.bundleIsFile, "bundle-is-file", "f", false, "Indicates that the bundle source is a file path")
-	f.BoolVarP(&export.ociLayout, "oci-layout", "l", false, "Export images as an OCI image layout")
 	f.BoolVarP(&export.thin, "thin", "t", false, "Export only the bundle manifest")
 	f.BoolVarP(&export.verbose, "verbose", "v", false, "Verbose output")
 
@@ -92,7 +80,7 @@ func (ex *exportCmd) run() error {
 }
 
 func (ex *exportCmd) Export(bundlefile string, l loader.BundleLoader) error {
-	is, err := packager.NewImageStore(ex.thin, ex.ociLayout)
+	is, err := packager.NewImageStore(ex.thin, true)
 	if err != nil {
 		return err
 	}
