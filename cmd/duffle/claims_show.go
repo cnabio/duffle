e1ca5d87d6c37a3f5344bdb16647f228efd8a3f0
Urvashi Reddy <ureddy@pivotal.io>
2019-08-12 18:51:26 -0700

    Update to support outputs in claims.
    - Bump to cnab-go released version where outputs are implemented.
    - Add --output flag to claim show.
    - Run flag validation in PreRunE hook.
    - Uses subtests to separate happy path from error cases.
    
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- cmd/duffle/claims_show.go
+++ cmd/duffle/claims_show.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 
 	"github.com/deislabs/cnab-go/claim"
@@ -15,42 +17,72 @@ Display the content of a claim.
 This dumps the entire content of a claim as a JSON object.
 `
 
+type claimsShowCmd struct {
+	Name       string
+	OnlyBundle bool
+	Output     string
+	Storage    claim.Store
+}
+
 func newClaimsShowCmd(w io.Writer) *cobra.Command {
-	var onlyBundle bool
+	var cmdData claimsShowCmd
 	cmd := &cobra.Command{
 		Use:     "show NAME",
 		Short:   "show a claim",
 		Long:    claimsShowDesc,
 		Aliases: []string{"get"},
 		Args:    cobra.ExactArgs(1),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return cmdData.validateShowFlags()
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			name := args[0]
-			storage := claimStorage()
-			return displayClaim(name, w, storage, onlyBundle)
+			cmdData.Name = args[0]
+			cmdData.Storage = claimStorage()
+			return cmdData.runClaimShow(w)
 		},
 	}
 
-	cmd.Flags().BoolVarP(&onlyBundle, "bundle", "b", false, "only show the bundle from the claim")
+	cmd.Flags().BoolVarP(&cmdData.OnlyBundle, "bundle", "b", false, "only show the bundle from the claim")
+	cmd.Flags().StringVarP(&cmdData.Output, "output", "o", "", "show the contents of the named output")
 
 	return cmd
 }
 
-func displayClaim(name string, out io.Writer, storage claim.Store, onlyBundle bool) error {
-	c, err := storage.Read(name)
+func (csc claimsShowCmd) validateShowFlags() error {
+	if csc.OnlyBundle && csc.Output != "" {
+		return errors.New("invalid flags: at most one of --bundle and --output can be specified")
+	}
+	return nil
+}
+
+func (csc claimsShowCmd) runClaimShow(w io.Writer) error {
+	c, err := csc.Storage.Read(csc.Name)
 	if err != nil {
 		return err
 	}
 
-	var data []byte
-	if onlyBundle {
-		data, err = json.MarshalIndent(c.Bundle, "", "  ")
-	} else {
-		data, err = json.MarshalIndent(c, "", "  ")
+	if csc.OnlyBundle {
+		return displayAsJSON(w, c.Bundle)
 	}
-	if err != nil {
+
+	if csc.Output != "" {
+		output, found := c.Outputs[csc.Output]
+		if !found {
+			return fmt.Errorf("unknown output name: %s", csc.Output)
+		}
+
+		_, err := fmt.Fprint(w, output)
 		return err
 	}
 
+	return displayAsJSON(w, c)
+}
+
+func displayAsJSON(out io.Writer, v interface{}) error {
+	data, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return err
+	}
 	_, err = out.Write(data)
 	out.Write([]byte("\n"))
 	return err
