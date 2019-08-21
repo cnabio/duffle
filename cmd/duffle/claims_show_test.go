313847b1f9aee5f1e2a3336f28ac45e28a8b00c1
Leah Hanson <astrieanna@gmail.com>
2019-08-21 07:48:40 -0700

    Update to support outputs in claims. (#815)
    - Bump to cnab-go released version where outputs are implemented.
    - Add --output flag to claim show.
    - Run flag validation in PreRunE hook.
    - Uses subtests to separate happy path from error cases.
    
    Signed-off-by: Leah Hanson <lhanson@pivotal.io>

--- cmd/duffle/claims_show_test.go
+++ cmd/duffle/claims_show_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"testing"
 
 	"github.com/deislabs/cnab-go/claim"
@@ -11,50 +12,190 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDisplayClaim(t *testing.T) {
+func TestRunClaimShow(t *testing.T) {
 	var buf bytes.Buffer
-	storage := mockClaimStore()
 
-	storage.Store(claim.Claim{
-		Name: "myclaim",
-		Bundle: &bundle.Bundle{
-			Name:    "mybundle",
-			Version: "0.1.2",
-		},
+	t.Run("happy path", func(t *testing.T) {
+		csc := claimsShowCmd{
+			Name:       "myclaim",
+			OnlyBundle: false,
+			Storage:    mockClaimStore(),
+		}
+		expectedClaim := claim.Claim{
+			Name: "myclaim",
+			Bundle: &bundle.Bundle{
+				Name:    "mybundle",
+				Version: "0.1.2",
+				Outputs: map[string]bundle.Output{
+					"some-output": {
+						Path: "/some-output-path",
+					},
+				},
+			},
+			Outputs: map[string]interface{}{
+				"some-output": "some output contents",
+			},
+		}
+		csc.Storage.Store(expectedClaim)
+		err := csc.runClaimShow(&buf)
+		assert.NoError(t, err)
+
+		var got claim.Claim
+		if err := json.Unmarshal(buf.Bytes(), &got); err != nil {
+			t.Fatalf("Attempted to unmarshal claim, but got %s", err)
+		}
+		assert.Equal(t, expectedClaim, got)
 	})
 
-	displayClaim("myclaim", &buf, storage, false)
+	t.Run("error case: when storage returns error", func(t *testing.T) {
+		csc := claimsShowCmd{
+			Name:       "unknownclaim",
+			OnlyBundle: false,
+			Storage:    mockClaimStore(),
+		}
 
-	var got claim.Claim
-	if err := json.Unmarshal(buf.Bytes(), &got); err != nil {
-		t.Fatal(err)
-	}
-
-	is := assert.New(t)
-	is.Equal("myclaim", got.Name)
-	is.Equal("mybundle", got.Bundle.Name)
+		err := csc.runClaimShow(&buf)
+		assert.EqualError(t, err, "not found")
+	})
 }
 
-func TestDisplayClaim_Bundle(t *testing.T) {
+// happy path: when --bundle is specified
+func TestRunClaimShow_Bundle(t *testing.T) {
 	var buf bytes.Buffer
-	storage := mockClaimStore()
 
-	storage.Store(claim.Claim{
+	csc := claimsShowCmd{
+		Name:       "myclaim",
+		OnlyBundle: true,
+		Storage:    mockClaimStore(),
+	}
+	expectedClaim := claim.Claim{
 		Name: "myclaim",
 		Bundle: &bundle.Bundle{
 			Name:    "mybundle",
 			Version: "0.1.2",
+			Outputs: map[string]bundle.Output{
+				"some-output": {
+					Path: "/some-output-path",
+				},
+			},
 		},
-	})
-
-	displayClaim("myclaim", &buf, storage, true)
+		Outputs: map[string]interface{}{
+			"some-output": "some output contents",
+		},
+	}
+	csc.Storage.Store(expectedClaim)
+	err := csc.runClaimShow(&buf)
+	assert.NoError(t, err)
 
 	var got bundle.Bundle
 	if err := json.Unmarshal(buf.Bytes(), &got); err != nil {
-		t.Fatal(err)
+		t.Fatalf("Attempted to unmarshal bundle, but got %s", err)
+	}
+	assert.Equal(t, *expectedClaim.Bundle, got)
+}
+
+func TestRunClaimShow_Output(t *testing.T) {
+	var buf bytes.Buffer
+	expectedClaim := claim.Claim{
+		Name: "myclaim",
+		Bundle: &bundle.Bundle{
+			Name:    "mybundle",
+			Version: "0.1.2",
+			Outputs: map[string]bundle.Output{
+				"some-output": {
+					Path: "/some-output-path",
+				},
+			},
+		},
+		Outputs: map[string]interface{}{
+			"some-output": "some output contents",
+		},
 	}
 
-	is := assert.New(t)
-	is.Equal("mybundle", got.Name)
-	is.Equal("0.1.2", got.Version)
+	t.Run("happy path", func(t *testing.T) {
+		csc := claimsShowCmd{
+			Name:    "myclaim",
+			Output:  "some-output",
+			Storage: mockClaimStore(),
+		}
+		csc.Storage.Store(expectedClaim)
+
+		err := csc.runClaimShow(&buf)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedClaim.Outputs["some-output"], buf.String())
+	})
+
+	t.Run("error case: when --output name is an unknown output", func(t *testing.T) {
+		csc := claimsShowCmd{
+			Name:    "myclaim",
+			Output:  "not-an-output",
+			Storage: mockClaimStore(),
+		}
+		csc.Storage.Store(expectedClaim)
+
+		err := csc.runClaimShow(&buf)
+		assert.EqualError(t, err, "unknown output name: not-an-output")
+	})
+
+}
+
+//error case: when --bundle and --output are both specified
+func TestRunClaimShow_InvalidFlags(t *testing.T) {
+	csc := claimsShowCmd{
+		Name:       "myclaim",
+		Output:     "some-output",
+		OnlyBundle: true,
+		Storage:    mockClaimStore(),
+	}
+
+	err := csc.validateShowFlags()
+	assert.EqualError(t, err, "invalid flags: at most one of --bundle and --output can be specified")
+}
+
+func TestDisplayJSON(t *testing.T) {
+	buf := bytes.NewBuffer([]byte{})
+	expectedClaim := claim.Claim{
+		Name: "myclaim",
+		Bundle: &bundle.Bundle{
+			Name:    "mybundle",
+			Version: "0.1.2",
+			Outputs: map[string]bundle.Output{
+				"some-output": {
+					Path: "/some-output-path",
+				},
+			},
+		},
+		Outputs: map[string]interface{}{
+			"some-output": "some output contents",
+		},
+	}
+
+	t.Run("happy path", func(t *testing.T) {
+		err := displayAsJSON(buf, expectedClaim)
+		assert.NoError(t, err)
+
+		var got claim.Claim
+		if err := json.Unmarshal(buf.Bytes(), &got); err != nil {
+			t.Fatalf("Attempted to unmarshal claim, but got %s", err)
+		}
+		assert.Equal(t, expectedClaim, got)
+	})
+
+	t.Run("error case: can't marshal claim", func(t *testing.T) {
+		err := displayAsJSON(buf, func() {})
+		assert.EqualError(t, err, "json: unsupported type: func()")
+	})
+
+	t.Run("error case: can't write to output", func(t *testing.T) {
+		err := displayAsJSON(mockWriter{err: errors.New("failed to write")}, expectedClaim)
+		assert.EqualError(t, err, "failed to write")
+	})
+}
+
+type mockWriter struct {
+	err error
+}
+
+func (mw mockWriter) Write([]byte) (int, error) {
+	return 0, mw.err
 }
